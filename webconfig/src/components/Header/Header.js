c497591099d0f75635707dae6c0def5d2b33b8f9
Thomas C <t.cardonne@gmail.com>
2019-01-21 11:25:39 +0100

    [WebConfig] Fixed dependencies

--- webconfig/src/components/Header/Header.js
+++ webconfig/src/components/Header/Header.js
@@ -7,13 +7,10 @@ class Header extends Component {
   render() {
     return (
         <div className="header">
-            <Link to="/" className="header-title"><h1>RentCar</h1></Link>
+            <Link to="/" className="header-title"><h1>Grape</h1></Link>
 
             <div className="header-nav">
                 <Link to="/">Home</Link>
-                <Link to="/rent">Rent a car</Link>
-                <Link to="/return">Return a car</Link>
-                <Link to="/manage">Manage cars</Link>
             </div>
         </div>
     );
